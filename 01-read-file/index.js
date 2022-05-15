acb13804e0d58e0357212d46cdbcc0117a46b77b
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-15 14:51:28 +0300

    fix: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, '../01-read-file/text.txt'), 'utf8', function(error,data){
+fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', function(error,data){
   if(error) throw error; 
   console.log(data); 
 });
+
