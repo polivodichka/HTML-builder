b9463fef2da116369536a15799bfc226689a68c9
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-15 19:27:29 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
+  .then(removeOldFiles('files-copy'))
+  .then(copyFiles('files', 'files-copy'));
+
+async function removeOldFiles(dirname){
+  fs.readdir(path.join(__dirname, dirname))
+    .then((files) => {
+      files.forEach(file => {
+        fs.unlink(path.join(__dirname, dirname, file));
+      });
+    });
+}
+async function copyFiles(source, destination){
+  fs.readdir(path.join(__dirname, source))
+    .then((files) => {
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, source, file), path.join(__dirname, destination, file), 2);
+      });
+    });
+}
 
