38e3ff18c194339584d4117f63c85902ac5dd6ac
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-16 15:12:19 +0300

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+let writeableStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+const styleFolderName = 'styles';
+
+fsPromises.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '').then(() => {
+  // читаем файлы из styles
+  fsPromises.readdir(path.join(__dirname, styleFolderName), { withFileTypes: true })
+    .then((files) => {
+      files.forEach(file => {
+        //если файл стилей
+        if (file.isFile() && path.extname(file.name) === '.css') {
+          let readableStream = fs.createReadStream(path.join(__dirname, styleFolderName, file.name), 'utf8');
+          //получаем содержимое
+          readableStream.on('data', (text) => {
+            //и записываем
+            writeableStream.write(text);
+          });
+        }
+      });
+    });
+});
+
+process.on('exit', () => {
+  console.log('Successful!');
+});
+
+
+
+
+
