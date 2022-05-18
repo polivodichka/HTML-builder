7a7569a8262142a4ff22d24a322dfe06bb65a888
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-18 09:51:00 +0300

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,16 +2,16 @@ const fsPromises = require('fs/promises');
 const path = require('path');
 
 fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
-  .then(removeOldFiles('files-copy'))
-  .then(copyFiles('files', 'files-copy'));
+  .then(async () => removeOldFiles('files-copy'))
+  .then(async () => copyFiles('files', 'files-copy'));
 
 async function removeOldFiles(dirname){
-  fsPromises.readdir(path.join(__dirname, dirname), { withFileTypes: true })
-    .then((files) => {
-      files.forEach(file => {
-        if(file.isFile()) fsPromises.unlink(path.join(__dirname, dirname, file.name));
-        else fsPromises.rmdir(path.join(__dirname, dirname, file.name), { recursive: true, force: true });
-      });
+  await fsPromises.readdir(path.join(__dirname, dirname), { withFileTypes: true })
+    .then(async (files) => {
+      for await (const file of files){
+        if(file.isFile()) await fsPromises.unlink(path.join(__dirname, dirname, file.name));
+        else await fsPromises.rmdir(path.join(__dirname, dirname, file.name), { recursive: true, force: true });
+      }
     });
 }
 async function copyFiles(source, destination){
@@ -22,7 +22,7 @@ async function copyFiles(source, destination){
           await fsPromises.copyFile(path.resolve(__dirname, source, file.name), path.resolve(__dirname, destination, file.name), 2);
         }
         else {
-          fsPromises.mkdir(path.resolve(__dirname, destination, file.name), { recursive: true });
+          await fsPromises.mkdir(path.resolve(__dirname, destination, file.name), { recursive: true });
           await copyFiles(path.resolve(__dirname, source, file.name), path.resolve(__dirname, destination, file.name));
         }
       }
